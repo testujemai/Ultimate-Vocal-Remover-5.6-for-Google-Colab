ddb9e49a3a9f4623d7ac79842403df8dc11ca2e7
testujemai <164744062+testujemai@users.noreply.github.com>
2024-06-20 08:34:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,24 @@
 import os
 
-os.system("python webUI.py")
+%cd /kaggle/working/ai-voice-cloning
+Ngrok_token = "2hF4nM18BPxdRnYlIwUw9hae9r3_7uBDMDgctvhhJZb4SEJKu" #@param {type:"string"}
+# Put your ngrok token here (obtainable from https://ngrok.com)
+
+Ngrok_domain = "" # optional, leave empty if you don't have a domain
+
+# -------------------------------- #
+
+!pip install pyngrok
+
+from pyngrok import ngrok, conf
+import fileinput
+import sys
+
+if Ngrok_token!="":
+  ngrok.kill()
+  srv=ngrok.connect(7860 , pyngrok_config=conf.PyngrokConfig(auth_token=Ngrok_token),
+                    bind_tls=True, domain=Ngrok_domain).public_url
+  print(srv)
+  get_ipython().system("python webUI.py")
+else:
+  print('An ngrok token is required. You can get one on https://ngrok.com and paste it into the ngrok_token field.')
